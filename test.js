c96f80b1b6117ec6dc7ed67435515600197036d2
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-11-03 16:58:30 +0900

    백준 input 형식

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.log(s.slice(0, 1));
-console.log(s.at(-1));
+const input = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(/\r?\s/);
